796bc2045185f224d1cd9f1408b95ae5ae1e491a
aurpur <ikamaaurel@gmail.com>
2023-07-12 15:53:53 +0200

    Update parser

--- parse/parse.py
+++ parse/parse.py
@@ -1,19 +1,36 @@
 import ast
 import json
+
 from ast2json import ast2json
 
+############################################# 
+## Transform the Python sample to AST tree ## 
+## And save it to JSON file                ##
+#############################################
+
+
+## Todo : change with the needed sample
+subFolder = "layers_formation/"
+filename = "gh_non_dominating_dow_sampling.py"
+
+## Sample folder and output folder
 sampleFolder = "../sample_design_smell/"
-filename = "layers_formation/gh_non_dominating_dow_sampling.py"
-path = sampleFolder + filename
+outputFolder = "output/"
 
+## Build the path of the sample and the output file
+path = sampleFolder + subFolder + filename
+outputFile = outputFolder + filename.split('.')[0]+ ".json"
 
+## Parse the Python sample to AST tree
 f = open(path, "r")
 tree = ast.parse(f.read())
 
-resultFile = open("output/result.json", "w")
+## Save the AST tree to JSON file
+resultFile = open(outputFile, "w")
 print(ast2json(tree))
 resultFile.write(json.dumps(ast2json(tree)))
 
+## Close the files stream
 f.close()
 resultFile.close()
 
